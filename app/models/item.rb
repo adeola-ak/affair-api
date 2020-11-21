347e3aad31b2f0a3af45317c33303c713537bc56
adeola <adeola@Adeolas-MBP.fios-router.home>
2020-11-21 02:26:36 -0500

    ensuring all changs captured

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
-  validates :designer, :type, :color, :presence => true
+  validates :designer, :item_type, :color, :presence => true
 
 end
-
-      t.string :designer
-      t.string :type
-      t.string :subtype
-      t.string :description
-      t.string :color
-      t.string :season
-      t.string :url
-      t.boolean :favorite
-      t.references :user, null: false, foreign_key: true
