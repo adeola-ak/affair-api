98aff42a6646284df57cd993149d9701f34b4529
adeola <adeola@Adeolas-MBP.fios-router.home>
2020-11-23 11:01:31 -0500

    checking for untracked changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,24 +19,23 @@ Item.create(designer: "Topshop", item_type: "Jacket", subtype: "Leather Jacket",
 Item.create(designer: "Jefferey Campbell", item_type: "Heels", subtype: "Wedge Mule", description: "Clear Wedge Heel", color: "Clear", season: "Summer 2020", url: "https://n.nordstrommedia.com/id/sr3/84614aa7-2e56-4f96-8303-9aa5ace9a76a.webp?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=60&h=90&dpr=2", favorite: true, user_id: 1)
 
 
-Item.create(designer: "Topshop", item_type: "Dress", subtype: "Short Dress", description: "Ruched Satin Dress", color: "Black", season: "Summer 2020", url: "https://n.io.nordstrommedia.com/id/sr3/4e6898de-b4e6-4d5c-a682-f363c91dab32.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90", favorite: true, user_id: 1)
+Item.create(designer: "Topshop", item_type: "Dress", subtype: "Short Dress", description: "Ruched Satin Dress", color: "Black", season: "Summer 2020", url: "https://n.io.nordstrommedia.com/id/sr3/4e6898de-b4e6-4d5c-a682-f363c91dab32.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196&dpr=2", favorite: true, user_id: 1)
 
-Item.create(designer: "Topshop", item_type: "Top", subtype: "Crop Top", description: "Square Neck Printed Crop Top", color: "White", season: "Spring 2020", url: "https://n.io.nordstrommedia.com/id/sr3/3525059c-d51c-422a-8223-d7d3cf6f07ec.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90", favorite: true, user_id: 1)
+Item.create(designer: "Topshop", item_type: "Top", subtype: "Crop Top", description: "Square Neck Printed Crop Top", color: "White", season: "Spring 2020", url: "https://n.io.nordstrommedia.com/id/sr3/3525059c-d51c-422a-8223-d7d3cf6f07ec.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196&dpr=2", favorite: true, user_id: 1)
 
 
-Item.create(designer: "Topshop", item_type: "Top", subtype: "Crop Top", description: "Square Neck Printed Crop Top", color: "White", season: "Spring 2020", url: "https://n.io.nordstrommedia.com/id/sr3/3525059c-d51c-422a-8223-d7d3cf6f07ec.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90", favorite: true, user_id: 1)
+Item.create(designer: "Jefferey Campbell", item_type: "Heels", subtype: "Platform Heel", description: "Open Toe Platform Heel", color: "Black", season: "Summer 2020", url: "https://n.io.nordstrommedia.com/id/sr3/236208d5-b879-4d35-bf25-bcde61a328db.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2", favorite: true, user_id: 1)
 
-Item.create(designer: "Topshop", item_type: "Top", subtype: "Camisole", description: "Lace Up Sheer Printed Cami", color: "Black", season: "Summer 2020", url: "https://n.io.nordstrommedia.com/id/sr3/5d55abcb-6ab1-43a2-b152-81c4702fde43.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90", favorite: true, user_id: 1)
+Item.create(designer: "Topshop", item_type: "Top", subtype: "Camisole", description: "Lace Up Sheer Printed Cami", color: "Black", season: "Summer 2020", url: "https://n.io.nordstrommedia.com/id/sr3/5d55abcb-6ab1-43a2-b152-81c4702fde43.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196&dpr=2", favorite: true, user_id: 1)
 
 # Item.create(designer: "", item_type: "", subtype: "", description: "", color: "", season: "Summer 2020", url:  favorite: true, user_id: 1)
 
-Item.create(designer: "Prada", item_type: "Sneaker", subtype: "", description: "Black With Red Logo", color: "Black", season: "Summer 2019", url: "https://n.io.nordstrommedia.com/id/sr3/bf84bc34-e429-49e9-8113-d577162f81fc.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=60&h=90", favorite: true, user_id: 1)
+Item.create(designer: "Prada", item_type: "Sneaker", subtype: "", description: "Black With Red Logo", color: "Black", season: "Summer 2019", url: "https://n.io.nordstrommedia.com/id/sr3/bf84bc34-e429-49e9-8113-d577162f81fc.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2", favorite: true, user_id: 1)
 
-Item.create(designer: "Off-White", item_type: "Handbag", subtype: "Crossbody", description: "Cash Inside Printed Bag", color: "Black", season: "Summer 2019", url: "https://n.io.nordstrommedia.com/id/sr3/793455cf-7c07-403f-8b4d-24602315e513.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=60&h=90",  favorite: true, user_id: 1)
+Item.create(designer: "Off-White", item_type: "Handbag", subtype: "Crossbody", description: "Cash Inside Printed Bag", color: "Black", season: "Summer 2019", url: "https://n.io.nordstrommedia.com/id/sr3/793455cf-7c07-403f-8b4d-24602315e513.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2",  favorite: true, user_id: 1)
 
-Item.create(designer: "Marine Serre", item_type: "Hat", subtype: "Bucket Hat", description: "Denim Bucket Hat", color: "Blue", season: "Summer 2020", url: "https://n.io.nordstrommedia.com/id/sr3/45a076b1-d60b-49b3-bdf8-1da91eea7fb3.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90",  favorite: true, user_id: 1)
-
-
+Item.create(designer: "Marine Serre", item_type: "Hat", subtype: "Bucket Hat", description: "Denim Bucket Hat", color: "Blue", season: "Summer 2020", url: "https://n.io.nordstrommedia.com/id/sr3/45a076b1-d60b-49b3-bdf8-1da91eea7fb3.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196&dpr=2",  favorite: true, user_id: 1)
 
 
 puts "seeded"
+
