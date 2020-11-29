dd718162f5347d678afcc14ebfbe5ce21a90245e
adeola <adeola@Adeolas-MBP.fios-router.home>
2020-11-28 23:55:17 -0500

    wow..idek...at all...

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 User.create(username: "digmydrip11", password: "mango", name: "adeola", gender: "F", style: "flashy")
 
-Item.create(designer: "Princess Polly", item_type: "Dress", subtype: "Sleeveless Dress", description: "Nude Mock Neck Tight Dress", color: "Nude", season: "Summer" year: 2020, url: "https://cdn.shopify.com/s/files/1/0061/8627/0804/products/3_3ff394aa-da0b-4280-9395-3e9a7f99b5c9_1800x.jpg?v=1596816133", favorite: false, user_id: 1)
+Item.create(designer: "Princess Polly", item_type: "Dress", subtype: "Sleeveless Dress", description: "Nude Mock Neck Tight Dress", color: "Nude", season: "Summer", year: 2020, url: "https://cdn.shopify.com/s/files/1/0061/8627/0804/products/3_3ff394aa-da0b-4280-9395-3e9a7f99b5c9_1800x.jpg?v=1596816133", favorite: false, user_id: 1)
 
 Item.create(designer: "Jefferey Campbell", item_type: "Boots", subtype: "Ankle Boots", description: "Block Heel Ankle Boots", color: "Olive-Green", season: "Fall", year: 2020, url: "https://cdn.shopify.com/s/files/1/1439/1602/products/maximal-lo-heeled-bootie-yyh-dark-green-6-746012_2400x.jpg?v=1601663348", favorite: true, user_id: 1)
 
